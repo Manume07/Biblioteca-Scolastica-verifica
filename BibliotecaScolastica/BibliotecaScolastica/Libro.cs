a60a51cfab1932d3d0716d808e46c1d86180cfe8
Manuel Campana <MANUEL.CAMPANA@local.ittcesena.it>
2025-11-21 08:22:05 +0100

    R0.2 aggiunto il costruttore

--- BibliotecaScolastica/BibliotecaScolastica/Libro.cs
+++ BibliotecaScolastica/BibliotecaScolastica/Libro.cs
@@ -8,5 +8,20 @@ namespace BibliotecaScolastica
 {
     internal class Libro
     {
+        public string Autore { get; set; }
+        public string Titolo { get; set; }
+        public string annoPubblicazione { get; set; }
+        public string Editore { get; set; }
+        public int NumeroPagine { get; set; }
+
+        public Libro(string _autore, string _titolo, string _annoPubblicazione, string _editore, int _numeroPagine)
+        {
+            Autore = _autore;
+            Titolo = _titolo;
+            annoPubblicazione = _annoPubblicazione;
+            Editore = _editore;
+            NumeroPagine = _numeroPagine;
+
+        }
     }
 }
