2b3631b65044db36c1dbd5e9a5f0f0fb8a5262a8
Manuel Campana <MANUEL.CAMPANA@local.ittcesena.it>
2025-11-21 09:03:25 +0100

    R0.2 aggiunta scelta dall'utente

--- BibliotecaScolastica/BibliotecaScolastica/Program.cs
+++ BibliotecaScolastica/BibliotecaScolastica/Program.cs
@@ -8,8 +8,21 @@ namespace BibliotecaScolastica
 {
     internal class Program
     {
+        //L’applicazione deve consentire le seguenti operazioni:
+        //aggiunta di un nuovo libro alla biblioteca;
+        //ricerca di un libro a partire dal titolo;
+        //ricerca di tutti i libri di uno specifico autore;
+        //determinazione del numero dei libri presenti
         static void Main(string[] args)
         {
+            Console.WriteLine("Benvenuto nella biblioteca!");
+            Console.WriteLine("Scegli un'opzione:");
+            Console.WriteLine("1. Aggiungi un nuovo libro");
+            Console.WriteLine("2. Cerca un libro inserendo titolo");
+            Console.WriteLine("3. Cerca i libri inserendo l'autore");
+            Console.WriteLine("4. Visualizza il numero totale dei libri");
+            
+
         }
     }
 }
