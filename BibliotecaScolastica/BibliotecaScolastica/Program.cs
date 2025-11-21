da4291efafa9fe33a7a3573b7f1b4d9897dc3af0
Manume_ <128533365+Manume07@users.noreply.github.com>
2025-11-21 09:39:42 +0100

    Aggiunto commento su Program.cs

--- BibliotecaScolastica/BibliotecaScolastica/Program.cs
+++ BibliotecaScolastica/BibliotecaScolastica/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+// COMMENTO INSERITO RANDOM
 namespace BibliotecaScolastica
 {
     internal class Program
